4ae7c52416f5b6cc71f08d3e9267d7ce6b595216
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-03-11 05:09:30 +0530

    Update Rank.ts

--- src/commands/General/Rank.ts
+++ src/commands/General/Rank.ts
@@ -34,7 +34,7 @@ export default class Command extends BaseCommand {
     } catch (err) {
       M.reply(`Profile Picture not Accessible of ${username}`);
       pfp =
-        "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg";
+        "https://telegra.ph/file/a6dffa1efe1cd645aab15.jpg";
     }
     const exp = (await this.client.getUser(user)).Xp;
     let role: string;
