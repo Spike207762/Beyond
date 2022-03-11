99f8a77f8ace12b440f19844745592d223de4f71
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-03-11 05:44:35 +0530

    Update T2Party.ts

--- src/commands/Pokemon/T2Party.ts
+++ src/commands/Pokemon/T2Party.ts
@@ -54,7 +54,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `You might want to check your pc.`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "🎆© HEY-MARIN BOT 2022🎆",
       buttons: buttons,
       headerType: 1,
     };
