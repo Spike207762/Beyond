b4edf9963919a0587eba16f332d1824a5c0a5805
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-03-11 06:40:45 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -225,6 +225,7 @@ ${this.client.config.prefix}Help5 [${this.client.config.prefix}h5]
 🏮 ${this.client.config.prefix}ᴄɪʀᴄʟᴇ
 🏮 ${this.client.config.prefix}shorturl [Your url]
 🏮 ${this.client.config.prefix}toimg [(tag)[sticker]]
+🏮 ${this.client.config.prefix}karaoke [term]
 ────────────────
 -M O D E R A T I O N 
 ${this.client.config.prefix}Ｈelp6 [${this.client.config.prefix}h6]
