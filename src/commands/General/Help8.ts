c80099006bf99f8e05fd00817e1ad5fbd47cdc19
CHEY <97988840+Chey-san@users.noreply.github.com>
2022-03-11 11:54:42 +0530

    Update Help8.ts

--- src/commands/General/Help8.ts
+++ src/commands/General/Help8.ts
@@ -1,9 +1,10 @@
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient from "../../lib/WAClient";
-import { ICommand, IParsedArgs, ISimplifiedMessage } from "../../typings";
-import { MessageType, proto } from "@adiwajshing/baileys";
-import request from "../../lib/request";
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import request from '../../lib/request'
+
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
