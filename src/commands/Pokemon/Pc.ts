fa206410226bdd767c7cba8020155ed5acbdbe60
Spike207762 <106523817+Spike207762@users.noreply.github.com>
2022-05-30 16:40:58 +0530

    Update Pc.ts

--- src/commands/Pokemon/Pc.ts
+++ src/commands/Pokemon/Pc.ts
@@ -46,7 +46,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "🚀SPIKE🧧",
       buttons: buttons,
       headerType: 1,
     };
