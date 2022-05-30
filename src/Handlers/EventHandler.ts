abe063eba575713cb48103c4f37861e01a9d4681
Spike207762 <106523817+Spike207762@users.noreply.github.com>
2022-05-30 15:16:12 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -53,7 +53,7 @@ export default class EventHandler {
       }
       const buttonMessage: any = {
         contentText: `${text}`,
-        footerText: "🎇 Beyond 🎇",
+        footerText: "🎇 SPIKE 🎇",
         buttons: buttons,
         headerType: 1,
       };
