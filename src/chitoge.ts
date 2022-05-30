b0022e327f67c581d18cefa02430ff04961c6140
Spike207762 <106523817+Spike207762@users.noreply.github.com>
2022-05-30 16:45:02 +0530

    Update chitoge.ts

--- src/chitoge.ts
+++ src/chitoge.ts
@@ -13,7 +13,7 @@ import AssetHandler from "./Handlers/AssetHandler";
 import EventHandler from "./Handlers/EventHandler";
 import NewsHandler from "./Handlers/NewsHandler";
 import ModsHandler from "./Handlers/ModsHandler";
-import ModsChecker from "./Checkers/ModsChecker";
+
 
 if (!process.env.MONGO_URI) throw new Error("MONGO URL IS NOT PROVIDED");
 const client = new WAClient({
@@ -38,7 +38,7 @@ const assetHandler = new AssetHandler(client);
 const eventHandler = new EventHandler(client);
 const newsHandler = new NewsHandler(client);
 const modsHandler = new ModsHandler(client);
-const modsChecker = new ModsChecker(client);
+
 messageHandler.loadCommands();
 assetHandler.loadAssets();
 messageHandler.loadFeatures();
@@ -116,7 +116,7 @@ db.once("open", () => {
     messageHandler.handleState();
     newsHandler.broadcastNews();
     messageHandler.spawnPokemon();
-    modsChecker.check();
+
     messageHandler.summonChara();
   });
 });
