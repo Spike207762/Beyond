4635a181b01e044ed3bae93089e64d36b2363f4d
Spike207762 <106523817+Spike207762@users.noreply.github.com>
2022-05-30 15:35:46 +0530

    Update Bank.ts

--- src/commands/Economy/Bank.ts
+++ src/commands/Economy/Bank.ts
@@ -28,7 +28,7 @@ export default class Command extends BaseCommand {
 
     const buttonMessage: any = {
       contentText: `🏦 *Bank | ${M.sender.username}*\n\n🪙 *Gold: ${result}*`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "🚀SPIKE🧧",
       buttons: buttons,
       headerType: 1,
     };
