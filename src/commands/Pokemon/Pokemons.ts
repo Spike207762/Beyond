93681cb82b45e8b81c254ca875880899d2b96e54
Spike207762 <106523817+Spike207762@users.noreply.github.com>
2022-05-30 16:41:38 +0530

    Update Pokemons.ts

--- src/commands/Pokemon/Pokemons.ts
+++ src/commands/Pokemon/Pokemons.ts
@@ -52,7 +52,7 @@ export default class Command extends BaseCommand {
     }
     const buttonMessage: any = {
       contentText: `${text}`,
-      footerText: "🎇 Beyond 🎇",
+      footerText: "🚀SPIKE🧧",
       buttons: buttons,
       headerType: 1,
     };
